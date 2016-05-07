19a125ba40b32167b8b619709aee23b86a294c71
Chance Smith <chance@sodiumhalogen.com>
2016-05-06 22:12:50 -0500

    change points to votes, space out data points

--- packages/telescope-posts/lib/client/templates/modules/post_info.js
+++ packages/telescope-posts/lib/client/templates/modules/post_info.js
@@ -1,5 +1,5 @@
 Template.post_info.helpers({
   pointsUnitDisplayText: function(){
-    return this.upvotes === 1 ? i18n.t('point') : i18n.t('points');
+    return this.upvotes === 1 ? i18n.t('vote') : i18n.t('votes');
   }
 });
